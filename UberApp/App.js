76e9a28b62b667174292d5243d4ec5933358f2ca
Numb246 <77034191+Numb246@users.noreply.github.com>
2022-05-15 00:14:44 +0700

    detailscreen

--- UberApp/App.js
+++ UberApp/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View} from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
 export default function App() {
   return (
     <View style={styles.container}>
-        <HomeScreen/>
+        <RestaurantDetailsScreen></RestaurantDetailsScreen>
         <StatusBar style="auto"/>
     </View>
   );
