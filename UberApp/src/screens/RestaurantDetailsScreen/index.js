0bc48fc46eebc74b6c6d11b6d7e94c8a2dc20a6c
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2022-05-15 21:50:47 +0700

    Menu List Screen
    - bị lỗi UI

--- UberApp/src/screens/RestaurantDetailsScreen/index.js
+++ UberApp/src/screens/RestaurantDetailsScreen/index.js
@@ -2,31 +2,21 @@
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
 import React from 'react'
 import {Ionicons} from '@expo/vector-icons'
+import DishListItem from '../../components/RestaurantItem/DishListItem';
 import restaurants from '../../../assets/data/restaurants.json';
+import {Header} from './Header';
 const restaurant = restaurants[0];
 ("");
 
 const RestaurantDetailsPage = () => {
   return (
     <View style={styles.page}>
-      <Image 
-      source={{uri: restaurant.image}} 
-      style={styles.image}
+      <FlatList 
+        ListHeaderComponent={Header}
+        data={restaurant.dishes}
+        renderItem={({item})=><DishListItem dish={item}/>}
       />
-      <Ionicons
-        name="arrow-back-circle"
-        size={45}
-        color="white"
-        style={styles.iconContainer}
-         />
 
-      <View style={styles.container}>
-        <Text style={styles.title}>{restaurant.name}</Text>
-        <Text style={styles.subtitle}>
-              $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-              {restaurant.maxDeliveryTime} minutes
-        </Text>
-      </View>
     </View>
   )
 }
